b40be1b2406b0aec61b4468d47264d22bacfc330
eqnat <nuclear.catapult@gmail.com>
2023-11-14 13:45:03 -0600

    no longer cleaning '*.agdai' files for every test

--- scripts/pre-commit.bash
+++ scripts/pre-commit.bash
@@ -2,8 +2,8 @@
 
 echo "$(dirname "$0")"
 
-#clean
-find "$(dirname "$0")"/.. -type f -name "*\.agdai" -exec rm {} \;
+##clean
+# find "$(dirname "$0")"/.. -type f -name "*\.agdai" -exec rm {} \;
 
 echo "Running pre-commit hook"
 git ls-files | grep -E "*\.agda" 2> /dev/null | while read line
